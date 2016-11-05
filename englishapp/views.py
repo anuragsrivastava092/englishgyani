3827a9c329455bfde10f606f2ba7979ec77685f1
agarg <agarg@algoscale.com>
2016-11-05 13:23:12 +0530

    chanbgs

--- englishapp/views.py
+++ englishapp/views.py
@@ -6,9 +6,9 @@ import json
 # Create your views here.
 class List_Article(View):
     def get(self,request):
-        # article_list=englishapi_view.Display_Article_LIst.as_view()(self.request).content
-        # article_list=json.loads(article_list)
-        return render(request,"article_list.html")
+        article_list=englishapi_view.Display_Article_LIst.as_view()(self.request).content
+        article_list=json.loads(article_list)
+        return render(request,"article_list.html",{"article_list":article_list})
 
 class Open_Article(View):
     def get(self,request,article_id):
