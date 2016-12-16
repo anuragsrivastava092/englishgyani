2cbb4f7b61d5fb0e81137105fd857cfa77d80a07
kamalkashyap13 <kamal.kashyap13@gmail.com>
2016-12-16 15:47:31 +0530

    front page redirection

--- englishapp/urls.py
+++ englishapp/urls.py
@@ -4,6 +4,7 @@ from englishapp import views as app_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$',app_views.Default.as_view(),name='default'),
     url(r'^article-list/', app_views.List_Article.as_view(),name='article-list'),
     url(r'^article-content/(?P<article_id>\d+)', app_views.Open_Article.as_view(),name='article-content'),
     url(r'^bookmark/', app_views.Bookmark_Words.as_view(),name='bookmark'),
